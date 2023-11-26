679ec3e98ada2d628dd36e1de04addeb3d5a17fe
carlos-martinez-maqueta <68261249+carlos-martinez-maqueta@users.noreply.github.com>
2023-11-25 20:03:20 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html lang="en">
+  <?php include 'head.php'; ?>
+  <body>
+    <?php include 'nav.php'; ?>
+    <main>
+      <!-- SECCION BANNER HOME -->
+      <section class="section_banner_home">
+        <div class="container">
+          <div class="row align-items-center justify-content-between">
+            <div class="col-lg-4 order_2_res">
+              <div class="info_banner">
+                <h2>¡SOMOS NXTEP!</h2>
+                <p>La primera comunidad de Producto y Growth que te conecta con los recursos y apoyo que necesitas para impulsar tu carrera.</p>
+
+                <div class="button_a_banner">
+                  <a href=""><img src="assets/img/comunidad.png" class="me-2">Únete a nuestra comunidad</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-6 order_1_res">
+              <div class="banners_imgs_card">
+                <img src="assets/img/banner.png" class="img-fluid">
+              </div>
+            </div>
+          </div>
+
+          <div class="row margin_top_op">
+            <div class="mb-lg-0 mb-3 col-lg-4 col-md-4 col-6">
+              <div class="card_op text-center">
+                <p id="porcentaje1">0%</p>
+                <span>Miembros sienten que han mejorado sus habilidades.</span>
+              </div>
+            </div>
+            <div class="mb-lg-0 mb-3 col-lg-4 col-md-4 col-6">
+              <div class="card_op text-center">
+                <p id="porcentaje2">0%</p>
+                <span>Miembros han hecho networking en el primer mes.</span>
+              </div>              
+            </div>
+            <div class="mb-lg-0 mb-3 col-lg-4 col-md-4 col-12">
+              <div class="card_op text-center">
+                <p id="porcentaje3">0%</p>
+                <span>Miembros han conseguido un trabajo en el mundo tech en menos de 6 meses.</span>
+              </div>              
+            </div>
+          </div>
+
+        </div>
+      </section>
+      <!-- SECCION NOSOTROS HOME -->
+      <section class="section_nosotros_home">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 titles_parrafo_n text-center mb-lg-4">
+              <h2>Descubre por qué deberías unirte a nosotros</h2>
+              <p>Nuestro objetivo es contribuir al desarrollo de la industria de tecnología del país y <br> próximamente LATAM. Y ¿Cómo lo hacemos?</p>
+            </div>
+
+            <div class="col-lg-3 card_n">
+              <div class="fondo_n">
+                <div class="text-center mb-lg-3 mb-3"><img src="assets/img/networking.png"></div>
+                <p>Networking</p>
+                <span>Conéctate con líderes de la industria y crea oportunidades de colaboración.</span>                
+              </div>
+            </div>
+            <div class="col-lg-3 card_n">
+              <div class="fondo_n">
+                <div class="text-center mb-lg-3 mb-3"><img src="assets/img/eventos.png"></div>
+                <p>Eventos y Recursos</p>
+                <span>Accede a eventos exclusivos y recursos para impulsar tu crecimiento.</span>                
+              </div>
+            </div>
+            <div class="col-lg-3 card_n">
+              <div class="fondo_n">
+                <div class="text-center mb-lg-3 mb-3"><img src="assets/img/aprendizaje.png"></div>
+                <p>Aprendizaje Continuo</p>
+                <span>Mantente actualizado y mejora constantemente tus habilidades.</span>                
+              </div>
+            </div>
+            <div class="col-lg-3 card_n">
+              <div class="fondo_n">
+                <div class="text-center mb-lg-3 mb-3"><img src="assets/img/desarrollo.png"></div>
+                <p>Desarrollo Profesional</p>
+                <span>Avanza en tu carrera y amplía tu conocimiento en Product y Growth.</span>                
+              </div>
+            </div>
+
+            <div class="col-lg-12 py-lg-5 py-2 text-center div_center_button">
+              <div class="buton_fondo"><a href="nosotros.php">Conoce más de nosotros <img src="assets/img/mentor.png" class="ms-3"></a></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- SECCION EVENTOS -->
+      <section class="voluntariado">
+        <div class="container">
+          <div class="row justify-content-between">
+            <div class="col-lg-6 div_titles_volun">
+              <h2>Próximos eventos gratuitos</h2>
+              <p>Se parte de nuestros eventos presenciales y online que tenemos para <br> ti, aprende más de Product & Growth, comparte tu experiencia en <br> este rubro y haz networking.</p>
+
+              <div class="buton_volun mt-lg-5"><a href="eventos.php"> Conoce más aquí <img src="assets/img/mentor.png" class="ms-3"></a></div>
+            </div>            
+            <div class="col-lg-5 text-center">
+              <img src="assets/img/voluntariado.png" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- SECCION MENTORES -->
+
+      <section class="mentores">
+        <div class="fondo_image">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12 title_mentoria text-center mb-lg-3">
+                <h2> <img src="assets/img/mentor.png"> Programa de Mentorías <img src="assets/img/mentor.png"> </h2>
+              </div>
+
+              <div class="col-lg-4 col-md-4 col-12 mb-lg-0 mb-3">
+                <div class="div_ment_1">
+                  <p>Aprende y crece con la ayuda de mentores</p>
+                  <span>El espacio que necesitabas para evolucionar tu perfil profesional y llevar tu carrera al siguiente nivel.</span>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-12 mb-lg-0 mb-3">
+                <div class="div_ment_2">
+                  <div class="d_flex_ment">
+                    <div>
+                      <p id="porcentaje4">+0</p>
+                      <span>Mentores de distintas <br> áreas.</span>
+                    </div>
+                    <div class="rigth_lad"><img src="assets/img/mentor-1.png"></div>
+                  </div>
+
+                  <div class="d_flex_ment_2">
+                    <div>
+                      <img src="assets/img/mentor-2.png">
+                    </div>
+                    <div class="ms-3 p_ment_2">
+                      <p>Mentores que trabajan en empresas que transforman la industria.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-12 mb-lg-0 mb-3">
+                <div class="div_ment_3">
+                  <p>¡Anímate a dar un paso más en tu carrera profesional!</p>
+
+                  <a href="mentorias.php">Conoce más aquí <img src="assets/img/flecha.png"></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+     <!-- SECCION VOLUNTARIOS -->
+      <section class="voluntariado">
+        <div class="container">
+          <div class="row justify-content-between">
+            <div class="col-lg-5 text-center">
+              <img src="assets/img/voluntariado.png" class="img-fluid d-lg-block d-none">
+            </div>
+            <div class="col-lg-6 div_titles_volun">
+              <h2>Dona tu talento como voluntario</h2>
+              <img src="assets/img/voluntariado.png" class="img-fluid d-lg-none d-block">
+              <p>Estamos en búsqueda de voluntarios que les gustaría ayudar en el crecimiento de la comunidad para así llevar más conocimiento de Growth y Product en todo el país y próximamente LATAM.</p>
+
+              <div class="buton_volun"><a href="voluntariado.php">Conoce más aquí <img src="assets/img/mentor.png" class="ms-3"></a></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    
+
+    <?php include 'footer.php'; ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="script.js"></script>
+
+
+    <script>
+      $(document).ready(function () {
+        var porcentaje1 = 60;
+        var porcentaje2 = 80;
+        var porcentaje3 = 30;
+        var porcentaje4 = 100;
+
+        animarPorcentaje('porcentaje1', porcentaje1);
+        animarPorcentaje('porcentaje2', porcentaje2);
+        animarPorcentaje('porcentaje3', porcentaje3);
+        animarPorcentajeConSigno('porcentaje4', porcentaje4);
+
+        function animarPorcentaje(elementId, porcentaje) {
+          var porcentajeActual = 0;
+          var intervalo = setInterval(function () {
+            $('#' + elementId).text(porcentajeActual + '%');
+            if (porcentajeActual >= porcentaje) {
+              clearInterval(intervalo);
+            }
+            porcentajeActual++;
+          }, 20);
+        }
+
+        function animarPorcentajeConSigno(elementId, porcentaje) {
+          var porcentajeActual = 0;
+          var intervalo = setInterval(function () {
+            $('#' + elementId).text('+' + porcentajeActual);
+            if (porcentajeActual >= porcentaje) {
+              clearInterval(intervalo);
+            }
+            porcentajeActual++;
+          }, 20);
+        }
+      });
+    </script>  
+  </body>
+</html>
---
+++ nosotros.php
@@ -0,0 +1,185 @@
+<!doctype html>
+<html lang="en">
+  <?php include 'head.php'; ?>
+
+  <body>
+
+    <?php include 'nav.php'; ?>
+
+    <main>
+
+      <section class="banner_nosotros">
+        <div class="position_ab_fondo"></div>
+        <img src="assets/img/banner_nosotros.png" class="w-100">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 floter_p">
+              <p>Creemos en tu potencial y acompañamos con personas tu ruta profesional</p>
+            </div>  
+          </div>
+        </div>
+      </section>
+
+      <section class="section_historia">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 text-center">
+              <h2>Nuestra historia NXTEP</h2>
+            </div>
+            <div class="col-lg-12">
+              <div class="flex_historys">
+                <div class="item_histo">
+                  <div class="text-center circle_naranja">2020</div>
+                  <div class="px-lg-5 px-3">
+                    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit,.</p>
+                  </div>
+                </div>
+                <div class="item_histo">
+                  <div class="text-center circle_naranja">2020</div>
+                  <div class="px-lg-5 px-3">
+                    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit,.</p>
+                  </div>
+                </div> 
+                <div class="item_histo">
+                  <div class="text-center circle_naranja">2020</div>
+                  <div class="px-lg-5 px-3">
+                    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit,.</p>
+                  </div>
+                </div> 
+                <div class="item_histo">
+                  <div class="text-center circle_naranja">2020</div>
+                  <div class="px-lg-5 px-3">
+                    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit,.</p>
+                  </div>
+                </div>                                                
+              </div>
+            </div>            
+          </div>
+        </div>
+      </section>
+
+
+      <section class="section_equipo">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 text-center">
+              <h3>¡Conoce a nuestro equipo!</h3>
+              <p>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+              <p>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-12">
+              <ul class="nav nav-pills my-lg-5" id="pills-tab" role="tablist">
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Founder & Chief of Staff</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Operations</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Marketing</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-disabled" type="button" role="tab" aria-controls="pills-disabled" aria-selected="false" >Community Building</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">People</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Mentoring</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-disabled" type="button" role="tab" aria-controls="pills-disabled" aria-selected="false" >Tecnología</button>
+                </li>                
+              </ul>
+              <div class="tab-content" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
+                  <div class="row">
+                  <?php
+                    $con = mysqli_connect("localhost","root","","db_nxtep");
+                    $sql = "SELECT * FROM tbl_user";
+                    $resultado = $con->query($sql);
+                    while ($fila = $resultado->fetch_assoc()) { ?>  
+                      <div class="col-lg-3">
+                        <div class="card_item_me">
+                          <div class="flex_price_star">
+                            <div><img src="assets/img/star.png" class="me-lg-2"><?php echo $fila['estrellas'] ?></div>
+                            <div class="text-end">S/<?php echo $fila['precio'] ?></div>
+                          </div>
+                          <div class="img_perfil_empresa text-center py-lg-4">
+                            <div class="position-relative">
+                              <div class="img_perfil_img"><img src="admin/<?php echo $fila['img_perfil'] ?>"></div>
+                              <div class="img_empresa"><img src="assets/img/empresa.png" class=""></div>
+                            </div>
+                          </div>
+
+                          <div class="datos_usuario text-center">
+                            <p><?php echo $fila['nombres'].'<br> '.$fila['apellidos'];  ?></p>
+                            <span><?php echo $fila['cargo'] ?> en <?php echo $fila['empresa'] ?></span>
+                          </div>
+                          <div class="separador_user_link"></div>
+
+                          <div class="redes_wb_links text-center">
+                            <ul>
+                              <li><a href="<?php echo $fila['linkedin'] ?>" target="_BLANCK"><img src="assets/img/lin-b.png"></a></li>
+                            </ul>
+
+                            <p>Experiencia: 3 años</p>
+                          </div>
+                        </div>
+                      </div>                                     
+                  <?php
+                      }
+                  ?>
+
+                    <!-- <div class="col-lg-3">
+                      <div class="card_item_me">
+                        <div class="flex_price_star">
+                          <div><img src="assets/img/star.png" class="me-lg-2">10</div>
+                          <div class="text-end">S/100</div>
+                        </div>
+                        <div class="img_perfil_empresa text-center py-lg-4">
+                          <div class="position-relative">
+                            <img src="assets/img/avatar.png">
+                            <div class="img_empresa"><img src="assets/img/empresa.png" class=""></div>
+                          </div>
+                        </div>
+                        <div class="datos_usuario text-center">
+                          <p>Nombre Apellido</p>
+                          <span>Product Lead en Rappi</span>
+                        </div>
+                        <div class="separador_user_link"></div>
+
+                        <div class="redes_wb_links text-center">
+                          <ul>
+                            <li><a href=""><img src="assets/img/lin-b.png"></a></li>
+                          </ul>
+
+                          <p>Experiencia: 3 años</p>
+                        </div>
+                      </div>
+                    </div>                    
+                  </div>-->
+
+                </div>
+                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">...</div>
+                <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0">...</div>
+                <div class="tab-pane fade" id="pills-disabled" role="tabpanel" aria-labelledby="pills-disabled-tab" tabindex="0">...</div>
+              </div>              
+            </div>            
+          </div>
+        </div>
+      </section>
+    </main>
+    
+
+    <?php include 'footer.php'; ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="script.js"></script>
+  </body>
+</html>
